1ecd80248f54d2fdc2d083f10d55ba7df44c7ca8
Cinthia Sidan <sidanusp@gmail.com>
2023-08-08 19:57:26 -0300

    Agregue la ruta para agregar un nuevo carrito

--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -10,7 +10,25 @@ router.get('/api/carritos', (req, res) => {
     res.json({ carts });
 });
 
-//
+// Ruta para crear un nuevo carrito
+router.post('/api/carritos', (req, res) => {
+    const { id, products } = req.body;
+
+    // Validar que se proporcionen los campos necesarios
+    if (!id || !products) {
+        return res.status(400).json({ error: 'Debe proporcionar un ID y un array de productos.' });
+    }
+
+    // Crear el nuevo carrito
+    const newCart = {
+        id,
+        products
+    };
+
+    carts.push(newCart);
+
+    res.json({ message: 'Carrito creado correctamente.' });
+});
 
 
 
